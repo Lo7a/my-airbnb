05d47d51afd46cc25e74fcf9f8048eb81718e1aa
Lo7a <lahavklugman7@gmail.com>
2022-10-31 18:16:50 +0200

    vers3

--- next.config.js
+++ next.config.js
@@ -4,42 +4,16 @@ const nextConfig = {
   swcMinify: true,
 };
 
-const securityHeaders = [
-  {
-    key: "X-Frame-Options",
-    value: "SAMEORIGIN",
-  },
-];
-
 module.exports = {
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: securityHeaders,
-      },
-    ];
+  images: {
+    domains: [
+      "https://links.papareact.com/5j2",
+      "https://links.papareact.com/isz/",
+      "links.papareact.com",
+    ],
   },
-};
-export const reactStrictMode = true;
-export async function headers() {
-  return [
-    {
-      // Apply these headers to all routes in your application.
-      source: "/(.*)",
-      headers: securityHeaders,
-    },
-  ];
-}
-
-export const images = {
-  domains: [
-    "https://links.papareact.com/5j2",
-    "https://links.papareact.com/isz/",
-    "links.papareact.com",
-  ],
-};
-export const env = {
-  mapbox_key:
-    "pk.eyJ1IjoibGFoYXYiLCJhIjoiY2w5dXR0bWNoMW84NDN3dGZoMGtnazZrOSJ9.r5Uq45QojDg-U6zTNhJiCw",
+  env: {
+    mapbox_key:
+      "pk.eyJ1IjoibGFoYXYiLCJhIjoiY2w5dXR0bWNoMW84NDN3dGZoMGtnazZrOSJ9.r5Uq45QojDg-U6zTNhJiCw",
+  },
 };
